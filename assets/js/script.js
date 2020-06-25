41d5d7ce88834839e03c0adeb2958928c413cd1f
AChigas <Achigas34@gmail.com>
2020-06-25 14:06:17 -0700

    added current weather data - HTML elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,38 @@ var forecastContainerEl = document.getElementById("forecast-container")
 
 var APIkey = "1f40db029d8aa5986ddf3ab9927c8d74";
 
-var displayCityData = function(city) {
+var displayCurrentData = function(city, temp, humidity, windSpeed, uvIndex, icon) {
+    currentContainerEl.textContent = ""
+    var divCityHeader = document.createElement("div")
     var headerCityDate = document.createElement("h2");
     var currentdate = moment().format("L");
+    var currentIcon = document.createElement("img");
+    currentIcon.setAttribute('src', "") 
+    currentIcon.setAttribute('src', "https://openweathermap.org/img/wn/" + icon + "@2x.png")
     headerCityDate.textContent = city + "   (" + currentdate + ")";
 
-    currentContainerEl.appendChild(headerCityDate)
+    divCityHeader.appendChild(headerCityDate)
+    divCityHeader.appendChild(currentIcon)
+    currentContainerEl.appendChild(divCityHeader)
+
+    var divCurrent = document.createElement("div")
+    var tempEl = document.createElement("p");
+    var humidityEl = document.createElement("p");
+    var windSpeedEl = document.createElement("p");
+    var UVIndexEl = document.createElement ("p");
+
+    tempEl.textContent = "Temperature: " + temp;
+    humidityEl.textContent = "Humidity: " + humidity;
+    windSpeedEl.textContent = "Wind Speed: " + windSpeed;
+    UVIndexEl.textContent = "UV Index: " + uvIndex;
+
+    
+    divCurrent.appendChild(tempEl);
+    divCurrent.appendChild(humidityEl);
+    divCurrent.appendChild(windSpeedEl);
+    divCurrent.appendChild(UVIndexEl);
+
+    currentContainerEl.appendChild(divCurrent);
     
 }
 
@@ -19,6 +45,8 @@ var displayCityData = function(city) {
 var getCityData = function() {
     event.preventDefault();
     var city = cityInputEl.value.trim()
+
+    //build out user input validation
 
     //current conditions in user-entered city//using it to get long and latitude for One call weather API url
     var cityInfoUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIkey;
@@ -34,13 +62,12 @@ var getCityData = function() {
         var latitude = data.coord.lat;
         var longitude = data.coord.lon;
 
-    displayCityData(cityName)
-    getWeatherData(latitude,longitude);
+    getWeatherData(cityName,latitude,longitude);
         });
     });
 };
 
-var getWeatherData = function(latitude,longitude) { 
+var getWeatherData = function(city,latitude,longitude) { 
     ///5-day forecast API
     var forecastUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&exclude=minutely,hourly&appid=" + APIkey;
         
@@ -50,27 +77,28 @@ var getWeatherData = function(latitude,longitude) {
 
         
         //Endpoints to dislay current data 
-        var tempCurrent = Math.floor(data.current.temp);
-        var humidity = data.current.humidity;
+        var tempCurrent = Math.round(data.current.temp);
+        var humidity = Math.round(data.current.humidity);
         var windSpeed = data.current.wind_speed;
         var uvIndex = data.current.uvi;
-        var iconCurrent = data.current.weather.icon;
+        var iconCurrent = data.current.weather[0].icon;
 
-        console.log(tempCurrent, humidity, windSpeed, uvIndex, iconCurrent)
+
+        displayCurrentData(city, tempCurrent, humidity, windSpeed, uvIndex, iconCurrent)
 
     for (var i=0; i < 5; i++)
-        var tempForecast = data.daily[i].temp;
+        var tempForecast = data.daily[i].temp.day;
         var humidityForecast = data.daily[i].humidity;
-        var iconForecast = data.daily[i].weather.icon;
+        var iconForecast = data.daily[i].weather[0].icon;
 
-        console.log(tempForecast, humidityForecast, iconForecast)
+        console.log(tempForecast[i])
 
         //endpoints to display 5 dat forecast cards
  
         //end response/then function 
         });
 
-    //end fetch/then function   
+    //end fetch/then function  
     });
 };
 
