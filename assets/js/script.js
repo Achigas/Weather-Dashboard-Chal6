2923873a61e30036219810a7a47aecd1d390f5a3
AChigas <Achigas34@gmail.com>
2020-06-25 17:00:46 -0700

    five day forecast data print

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,50 @@
 var cityInputEl = document.getElementById("city-input");
 var cityFormEl = document.getElementById("city-form");
 var searchEl = document.getElementById("search-button");
+var searchHistoryEl = document.getElementById("search-history")
 var currentContainerEl = document.getElementById("current-container")
 var forecastContainerEl = document.getElementById("forecast-container")
 
 var APIkey = "1f40db029d8aa5986ddf3ab9927c8d74";
 
-var displayCurrentData = function(city, temp, humidity, windSpeed, uvIndex, icon) {
+var displayCurrentData = function(city, data) {
+
+    //Endpoints to dislay current data 
+    var tempCurrent = Math.round(data.current.temp);
+    var humidity = Math.round(data.current.humidity);
+    var windSpeed = data.current.wind_speed;
+    var uvIndex = data.current.uvi;
+    var iconCurrent = data.current.weather[0].icon;
+
+    //create HTML for city/date/icon
     currentContainerEl.textContent = ""
     var divCityHeader = document.createElement("div")
     var headerCityDate = document.createElement("h2");
     var currentdate = moment().format("L");
-    var currentIcon = document.createElement("img");
-    currentIcon.setAttribute('src', "") 
-    currentIcon.setAttribute('src', "https://openweathermap.org/img/wn/" + icon + "@2x.png")
+    var imageIcon = document.createElement("img");
+    imageIcon.setAttribute('src', "") 
+    imageIcon.setAttribute('src', "https://openweathermap.org/img/wn/" + iconCurrent + "@2x.png")
     headerCityDate.textContent = city + "   (" + currentdate + ")";
 
+    //Append to container for current data
     divCityHeader.appendChild(headerCityDate)
-    divCityHeader.appendChild(currentIcon)
+    divCityHeader.appendChild(imageIcon)
     currentContainerEl.appendChild(divCityHeader)
 
+    //create element to display weather data
     var divCurrent = document.createElement("div")
     var tempEl = document.createElement("p");
     var humidityEl = document.createElement("p");
     var windSpeedEl = document.createElement("p");
     var UVIndexEl = document.createElement ("p");
 
-    tempEl.textContent = "Temperature: " + temp;
+    //add current weather data to page
+    tempEl.textContent = "Temperature: " + tempCurrent;
     humidityEl.textContent = "Humidity: " + humidity;
     windSpeedEl.textContent = "Wind Speed: " + windSpeed;
     UVIndexEl.textContent = "UV Index: " + uvIndex;
 
-    
+    //append elements to section
     divCurrent.appendChild(tempEl);
     divCurrent.appendChild(humidityEl);
     divCurrent.appendChild(windSpeedEl);
@@ -39,6 +52,45 @@ var displayCurrentData = function(city, temp, humidity, windSpeed, uvIndex, icon
 
     currentContainerEl.appendChild(divCurrent);
     
+}
+
+var displayForecastData = function(data) {
+    console.log(data)
+
+    for (var i=1; i < 6; i++) {
+        var tempForecast = Math.round(data.daily[i].temp.day);
+        var humidityForecast = data.daily[i].humidity;
+        var iconForecast = data.daily[i].weather[0].icon;
+
+    var cardEl = document.createElement("div");
+    cardEl.setAttribute("class","card");
+
+    var cardBodyEl = document.createElement("div");
+    cardBodyEl.setAttribute("class","card-body");
+
+    var cardDateEl = document.createElement("h6");
+    cardDateEl.textContent = "current date";
+
+    var cardIconEl = document.createElement("img");
+    cardIconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + iconForecast + "@2x.png")
+
+    var cardTempEl = document.createElement("p");
+    cardTempEl.setAttribute("class", "card-text");
+    cardTempEl.textContent = "Temperature:" + tempForecast + "°F";
+
+    var cardHumidEl = document.createElement("p")
+    cardHumidEl.setAttribute("class", "card-text");
+    cardHumidEl.textContent = "Humidity: " + humidityForecast + "%";
+
+    cardBodyEl.appendChild(cardDateEl)
+    cardBodyEl.appendChild(cardIconEl)
+    cardBodyEl.appendChild(cardTempEl)
+    cardBodyEl.appendChild(cardHumidEl)
+    
+    cardEl.appendChild(cardBodyEl);
+    forecastContainerEl.appendChild(cardEl);
+
+    }
 }
 
 
@@ -75,25 +127,9 @@ var getWeatherData = function(city,latitude,longitude) {
         response.json().then(function(data) {
             console.log(data);
 
-        
-        //Endpoints to dislay current data 
-        var tempCurrent = Math.round(data.current.temp);
-        var humidity = Math.round(data.current.humidity);
-        var windSpeed = data.current.wind_speed;
-        var uvIndex = data.current.uvi;
-        var iconCurrent = data.current.weather[0].icon;
+        displayCurrentData(city, data);
+        displayForecastData(data);
 
-
-        displayCurrentData(city, tempCurrent, humidity, windSpeed, uvIndex, iconCurrent)
-
-    for (var i=0; i < 5; i++)
-        var tempForecast = data.daily[i].temp.day;
-        var humidityForecast = data.daily[i].humidity;
-        var iconForecast = data.daily[i].weather[0].icon;
-
-        console.log(tempForecast[i])
-
-        //endpoints to display 5 dat forecast cards
  
         //end response/then function 
         });
